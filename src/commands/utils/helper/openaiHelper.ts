8879e5ac34c28a2c806cb731279ac542dd140e86
Andriy Grebenyuk <agrebenyuk@microsoft.com>
2023-09-12 16:11:22 +0000

    Moved OpenAI key to configuration

--- src/commands/utils/helper/openaiHelper.ts
+++ src/commands/utils/helper/openaiHelper.ts
@@ -1,10 +1,22 @@
 import OpenAI from 'openai';
-
-const openai = new OpenAI({
-  apiKey: process.env["OPENAI_API_KEY"]
-});
+import {failed } from '../errorable';
+import { getOpenAIConfig } from '../config';
+import * as vscode from 'vscode';
 
 export async function openaiHelper() {
+
+  const openaiConfig = getOpenAIConfig();
+  
+  if (failed(openaiConfig)) {
+    vscode.window.showInformationMessage(openaiConfig.error);
+    console.log(openaiConfig.error);
+    return
+  } 
+
+  const openai = new OpenAI({
+    apiKey: openaiConfig.result.apiKey
+  });
+
   const teststream = await openai.chat.completions.create({ messages: [{ role: 'user', content: 'How can I list all files in a directory using Python?' }], model: 'gpt-3.5-turbo', stream: true }, {
     timeout: 5 * 1000,
   });
